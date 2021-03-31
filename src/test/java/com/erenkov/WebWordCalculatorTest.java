4ecfde13bbc3274a0c42e50af151d8e8443e1ff2
aleksandr.erenkov <al.erenkov@yandex.ru>
2021-03-31 21:18:27 +0400

    build project tree

---
+++ src/test/java/com/erenkov/WebWordCalculatorTest.java
@@ -0,0 +1,20 @@
+package com.erenkov;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class WebWordCalculatorTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
